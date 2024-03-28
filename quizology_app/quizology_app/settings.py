b63e92dd892a688af6ddee7593ce37545c3788c7
maleekmalik@gmail.com <maleekmalik@gmail.com>
2024-03-28 22:25:37 +0300

    Add media file configuration

--- quizology_app/quizology_app/settings.py
+++ quizology_app/quizology_app/settings.py
@@ -125,6 +125,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Media files (user-uploaded files)
+# https://docs.djangoproject.com/en/5.0/topics/files/
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 

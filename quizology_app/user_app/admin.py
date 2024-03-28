49a58bd8397ea59d45f81d721599cae0f1e886aa
maleekmalik@gmail.com <maleekmalik@gmail.com>
2024-03-28 22:26:18 +0300

    Register Profile model in Django admin site

--- quizology_app/user_app/admin.py
+++ quizology_app/user_app/admin.py
@@ -1,3 +1,14 @@
-from django.contrib import admin
+"""
+This module is responsible for registering the Profile model in the Django admin site.
 
-# Register your models here.
+The Profile model represents user profiles in the application. By registering it with the admin site,
+administrators can manage user profiles through the Django admin interface.
+
+Usage:
+    - Import the Profile model from the models module.
+    - Register the Profile model with the admin site using the `admin.site.register()` function.
+"""
+from django.contrib import admin
+from .models import Profile
+
+admin.site.register(Profile)

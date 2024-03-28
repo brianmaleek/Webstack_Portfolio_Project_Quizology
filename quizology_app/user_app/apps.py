6cdfb3330ada11f11d1249bd40cf2d346fa106be
maleekmalik@gmail.com <maleekmalik@gmail.com>
2024-03-28 23:10:24 +0300

    Add import for signals module in UserAppConfig.ready() method

--- quizology_app/user_app/apps.py
+++ quizology_app/user_app/apps.py
@@ -4,3 +4,11 @@ from django.apps import AppConfig
 class UserAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'user_app'
+
+# 
+    def ready(self):
+        """
+        This method is called when the application is ready to handle requests.
+        It imports the signals module from the user_app package.
+        """
+        import user_app.signals
